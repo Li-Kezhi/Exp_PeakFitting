69cdd54fa3ccc9e397ad9d05ac09ac5a8d55b2fc
Li-Kezhi <lkz13@mails.tsinghua.edu.cn>
2017-01-24 17:54:18 +0800

    Make the script more user-friendly

--- Examples/PeakFitting/README.md
+++ Examples/PeakFitting/README.md
@@ -6,14 +6,8 @@ Two-column data file.
 
 Fitting plotting & plotting data. And also the fitting report.
 
-## Example 1
+## Example
 
 XRD fitting of two adjacent peaks of MnO2.
 
 Input file: Mn0-100.txt
-
-## Example 2
-
-XPS fitting of typical O 1s spectra.
-
-Input file: O1s.txt
--- PeakFitting.py
+++ PeakFitting.py
@@ -4,63 +4,89 @@
 Semi-auto fitting program
 
 Steps:
-1. File name and position
-    fileLocation, fileName
-2. Set spectrum range and background range
-    Cancel exit() before Background-part
+1. Set spectrum range and background range
 2. Background fitting
     Select background model and fit the data
-    Cancel exit() at the end of Background-part
     Set bg_ parameters
 3. Peaks information and initial guess
     Peak type, initial estimation
-    Cancel exit() and plotting codes after estimation-part
 4. Fitting
     Modify fitting parameters
-    Cancel last exit()
-    Do remember to change report/plotting format parameters
 5. Plotting and reports
 """
 
 __author__ = "LI Kezhi" 
-__date__ = "$2016-11-23$"
-__version__ = "1.2.1"
+__date__ = "$2017-01-24$"
+__version__ = "1.3.0"
 
 import numpy as np
-from lmfit.models import VoigtModel, LinearModel, PolynomialModel
+from lmfit.models import VoigtModel, PolynomialModel
 import matplotlib.pyplot as plt
 from scipy.integrate import simps
 
-fileLocation = './Examples/PeakFitting/'
-fileName = 'Mn0-100.txt'
+FILE_LOCATION = './Examples/PeakFitting/'
+FILE_NAME = 'Mn0-100.txt'
 
-start = 52.0      # Define the fitting range
-end = 64.0
+PLOT_HEAD = 52.0      # Define the fitting range
+PLOT_END = 64.0
 
 # Background setting
 BG_FITTING_MODE = 'z'   # 't' for two-point method;
                         # 'z' for two-zone method
 # Two-point method:
-head0 = 53   # Head x point
-end0 = 63
+BG_HEAD_TPM = 53   # Head x point
+BG_END_TPM = 63
 # Two-zone method:
-head1 = 52.1   # Head x range: head1 < x < head2
-head2 = 54
-end1 = 61
-end2 = 63.9
+BG_HEAD_LEFT_TZM = 52.1   # x range: BG_HEAD_LEFT_TZM < x < BG_HEAD_RIGHT_TZM
+BG_HEAD_RIGHT_TZM = 54
+BG_END_LEFT_TZM = 61
+BG_END_RIGHT_TZM = 63.9
+
+# Fitting parameters:
+BG_TYPE = 2  # 1: linear, 2: parabolic
+
+PEAK_NAMES = (
+    'p1_',
+    'p2_'
+)
+
+PEAK_CENTER = (
+    (56, None, 58),  # center, min, max
+    (59, 57, 61)
+)
+
+PEAK_AMPLITUDE = (
+    (2000, 0),  # amplitude, min
+    (2000, 0)
+)
+
+PEAK_SIGMA = (
+    (None, None, None),  # sigma, min, max
+    (None, None, None)
+)
+
+# Program Control:
+controlStatas = (
+    'Initial glimpse',
+    'Background fitting',
+    'Initial guess',
+    'Curve fitting',
+    'Report'
+)
+STATUS = controlStatas[4]  # Choose the current program status
 
 ###########################
 
 ##### Read data #####
-dat = np.loadtxt(fileLocation + fileName)
+dat = np.loadtxt(FILE_LOCATION + FILE_NAME)
 x_original = dat[:, 0]
 y_original = dat[:, 1]
 
 startLine, endLine = None, None
 for i in xrange(np.size(x_original)):
-    if x_original[i] >= start and startLine == None:
+    if x_original[i] >= PLOT_HEAD and startLine == None:
         startLine = i
-    if startLine != None and endLine == None and x_original[i] >= end:
+    if startLine != None and endLine == None and x_original[i] >= PLOT_END:
         endLine = i
         break
 if startLine == endLine:
@@ -76,18 +102,18 @@ y = y_original[startLine:endLine]
 
 
 ##### Original Data #####
-plt.plot(x, y, 'b.')
-plt.show() # First glimpse
-
-# exit()   # Stop here?
+if STATUS == 'Initial glimpse':
+    plt.plot(x, y, 'b.')
+    plt.show() # First glimpse
+    exit()
 
 ##### Background #####
 if BG_FITTING_MODE == 't':
     startLine, endLine = None, None
     for i in xrange(np.size(x_original)):
-        if x_original[i] >= head0 and startLine == None:
+        if x_original[i] >= BG_HEAD_TPM and startLine == None:
             startLine = i
-        if startLine != None and endLine == None and x_original[i] >= end:
+        if startLine != None and endLine == None and x_original[i] >= PLOT_END:
             endLine = i
     x_bg = [x_original[startLine], x_original[endLine]]
     y_bg = [y_original[startLine], y_original[endLine]]
@@ -95,20 +121,20 @@ elif BG_FITTING_MODE == 'z':
     startLine1, startLine2 = None, None
     endLine1, endLine2 = None, None
     for i in xrange(np.size(x_original)):
-        if x_original[i] >= head1 and startLine1 == None:
+        if x_original[i] >= BG_HEAD_LEFT_TZM and startLine1 == None:
             startLine1 = i
-        if startLine1 != None and startLine2 == None and x_original[i] >= head2:
+        if startLine1 != None and startLine2 == None and x_original[i] >= BG_HEAD_RIGHT_TZM:
             startLine2 = i
-        if x_original[i] >= end1 and endLine1 == None:
+        if x_original[i] >= BG_END_LEFT_TZM and endLine1 == None:
             endLine1 = i
-        if endLine1 != None and endLine2 == None and x_original[i] >= end2:
+        if endLine1 != None and endLine2 == None and x_original[i] >= BG_END_RIGHT_TZM:
             endLine2 = i
     x_bg = np.hstack((x_original[startLine1:startLine2], 
                                 x_original[endLine1:endLine2]))
     y_bg = np.hstack((y_original[startLine1:startLine2],
                                 y_original[endLine1:endLine2]))
 
-bg_mod = PolynomialModel(2, prefix='bg_')   # Background
+bg_mod = PolynomialModel(BG_TYPE, prefix='bg_')   # Background
 pars = bg_mod.guess(y_bg, x=x_bg)
 
 mod = bg_mod     
@@ -118,66 +144,93 @@ plt.plot(x, y, 'b.')
 
 out = mod.fit(y_bg, pars, x=x_bg)
 
-print(out.fit_report(min_correl=0.5))    # Parameter result
+if STATUS == 'Background fitting':
+    print(out.fit_report(min_correl=0.5))    # Parameter result
 
-plt.plot(x_bg, out.best_fit, 'r-')    # Background plotting
-plt.xlim([x[0], x[-1]])
-plt.show()
+    plt.plot(x_bg, out.best_fit, 'r-')    # Background plotting
+    plt.xlim([x[0], x[-1]])
+    plt.show()
 
-# exit()   # Stop here?
-pars['bg_c0'].set(-941.6)   # Set parameters after fitting bg
-pars['bg_c1'].set(34.81)
-pars['bg_c2'].set(-0.3106)
+    exit()
+
+pars = out.params
 
 ##### Peak Fitting #####
-peak1  = VoigtModel(prefix='p1_')    # Peak information
-pars.update(peak1.make_params())
+peaks = []
+peakParameters = {}
+for i, peak in enumerate(PEAK_NAMES):
+    peakParameters[peak] = {}
+    peakParameters[peak]['center'] = PEAK_CENTER[i]
+    peakParameters[peak]['amplitude'] = PEAK_AMPLITUDE[i]
+    peakParameters[peak]['sigma'] = PEAK_SIGMA[i]
+mod = bg_mod
+for i, peak in enumerate(PEAK_NAMES):
+    peaks.append(VoigtModel(prefix=peak))  # Peak information
+    pars.update(peaks[i].make_params())
 
-pars['p1_center'].set(56, min=54, max=58)
-pars['p1_amplitude'].set(2000, min=0)
+    pars[peak + 'center'].set(
+        peakParameters[peak]['center'][0],
+        min=peakParameters[peak]['center'][1], 
+        max=peakParameters[peak]['center'][2]
+        )
+    pars[peak + 'amplitude'].set(
+        peakParameters[peak]['amplitude'][0],
+        min=peakParameters[peak]['amplitude'][1]
+        )
+    pars[peak + 'sigma'].set(
+        peakParameters[peak]['sigma'][0],
+        min=peakParameters[peak]['sigma'][1],
+        max=peakParameters[peak]['sigma'][2]
+        )
 
-peak2  = VoigtModel(prefix='p2_')
-pars.update(peak2.make_params())
-
-pars['p2_center'].set(59, min=57, max=61)
-pars['p2_amplitude'].set(2000, min=0)
-
-mod = peak1 + peak2 + bg_mod      # Add-up
-
+    mod += peaks[i]
 
 init = mod.eval(pars, x=x)
 plt.plot(x, y, 'b.')
-# plt.plot(x, init, 'k--')   # Initial guess plotting
-# plt.show()   # Initial guess plotting
-# exit()   # Stop here?
+
+if STATUS == 'Initial guess':
+    plt.plot(x, init, 'k--')   # Initial guess plotting
+    plt.show()   # Initial guess plotting
+    exit()
 
 out = mod.fit(y, pars, x=x)
 
-print(out.fit_report(min_correl=0.5))    # Parameter result
-
 plt.plot(x, out.best_fit, 'r-')    # Graph result
 comps = out.eval_components(x=x)
-plt.plot(x, comps['bg_'], 'g-')            # Plot the background and the peaks
-plt.plot(x, comps['p1_'] + comps['bg_'],  'k-')
-plt.plot(x, comps['p2_'] + comps['bg_'],  'k-')
+plt.plot(x, comps['bg_'], 'g-')  # Plot the background and the peaks
+for i, peak in enumerate(PEAK_NAMES):
+    plt.plot(x, comps[peak] + comps['bg_'],  'k-')
 plt.show()
 
-# exit()   # Stop here?
+if STATUS == 'Curve fitting':
+    print(out.fit_report(min_correl=0.5))    # Parameter result
+    exit()   
 
 
 ##### Text output #####
 
-result_txt = open(fileLocation + 'result_' + fileName, 'w')
+result_txt = open(FILE_LOCATION + 'result_' + FILE_NAME, 'w')
 result_txt.write(out.fit_report(min_correl=0.5))
 result_txt.write('\n')
 result_txt.write('===================\n')
 comps = out.eval_components(x=x)
-area1 = simps(comps['p1_'], x)           # Integration results
-area2 = simps(comps['p2_'], x)
-result_txt.write('Area1 = ' + str(area1) + ', Area2 = ' + str(area2))
+
+for i, peak in enumerate(PEAK_NAMES):
+    area = simps(comps[peak], x)  # Integration results
+    result_txt.write('Area ' + str(i) + ': ' + str(area) + '\n')
+
 result_txt.close()
 
-graphFit = np.transpose(np.vstack((x, out.best_fit, comps['bg_'], comps['p1_'], comps['p2_'])))   # Fitting result
-np.savetxt(fileLocation + 'graph_' + fileName, graphFit, fmt = "%f, %f, %f, %f, %f", newline = '\n')
+resultFittingData = np.vstack((x, out.best_fit, comps['bg_']))
+headerStr = 'x  Fit  Background'
+for i, peak in enumerate(PEAK_NAMES):
+    resultFittingData = np.vstack((resultFittingData, comps[peak]))
+    headerStr += '  peak' + str(i)
+graphFit = np.transpose(resultFittingData)   # Fitting result
+np.savetxt(
+    FILE_LOCATION + 'graph_' + FILE_NAME, graphFit, newline = '\n',
+    header=headerStr
+)
 
+print('Successfully fitted and the report is generated!')
 
